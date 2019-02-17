8fb8491c3ff6068641e76fbdf99f60b03ad183bd
Chenng <ringcrl@foxmail.com>
2019-02-17 13:38:33 +0800

    notes: update dir

---
+++ depth-first-search/17.letter-combinations-of-a-phone-number.js
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=17 lang=javascript
+ *
+ * [17] Letter Combinations of a Phone Number
+ *
+ * https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
+ *
+ * algorithms
+ * Medium (40.06%)
+ * Total Accepted:    340.4K
+ * Total Submissions: 848.2K
+ * Testcase Example:  '"23"'
+ *
+ * Given a string containing digits from 2-9 inclusive, return all possible
+ * letter combinations that the number could represent.
+ * 
+ * A mapping of digit to letters (just like on the telephone buttons) is given
+ * below. Note that 1 does not map to any letters.
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * 
+ * Input: "23"
+ * Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+ * 
+ * 
+ * Note:
+ * 
+ * Although the above answer is in lexicographical order, your answer could be
+ * in any order you want.
+ * 
+ */
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+    
+};
